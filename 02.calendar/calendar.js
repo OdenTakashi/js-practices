ba671412cddc1b71cb19ebe6cab95d0b22ef3561
OdenTakashi <odentakashi@icloud.com>
2022-07-21 14:33:53 +0900

    今月の日付を出力するカレンダーを作成
    オプション指定の部分は未実装

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,5 +1,19 @@
-const dayjs = require('dayjs');
+const dayjs = require('dayjs')
 
-let date = dayjs(2010, 5, 0);
-console.log(date);
+const today = dayjs()
+const lastdate = today.endOf('month').date()
+const startweekday = today.day()
 
+console.log(`     ${today.month() + 1}月 ${today.year()}`)
+console.log(' 月 火 水 木 金 土 日')
+const startweekdayspace = '   '.repeat(startweekday)
+process.stdout.write(`   ${startweekdayspace}`)
+
+for (let date = 1; date <= lastdate; date++) {
+  const current = today.date(date)
+  const dayofweek = current.day()
+  if (dayofweek === 0) {
+    console.log('')
+  }
+  process.stdout.write(` ${String(date).padStart(2)}`)
+}
