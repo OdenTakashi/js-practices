b8215ef576f1856b40d00bb7d880e027cf24690b
OdenTakashi <odentakashi@icloud.com>
2022-07-22 12:58:46 +0900

    年と月を与えた時の処理を追加

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,18 +1,19 @@
 const dayjs = require('dayjs')
+const minimist = require('minimist')(process.argv.slice(2))
 
-const today = dayjs()
-const lastdate = today.endOf('month').date()
-const startweekday = today.day()
+const oneday = minimist.y ? dayjs(`${minimist.y}-${minimist.m}`) : dayjs()
+const lastdate = oneday.endOf('month').date()
+const startweekday = oneday.day()
 
-console.log(`     ${today.month() + 1}月 ${today.year()}`)
-console.log(' 月 火 水 木 金 土 日')
-const startweekdayspace = '   '.repeat(startweekday)
-process.stdout.write(`   ${startweekdayspace}`)
+console.log(`     ${oneday.month() + 1}月 ${oneday.year()}`)
+console.log(' 日 月 火 水 木 金 土')
+const startmargin = '   '.repeat(startweekday)
+process.stdout.write(`${startmargin}`)
 
 for (let date = 1; date <= lastdate; date++) {
-  const current = today.date(date)
-  const dayofweek = current.day()
-  if (dayofweek === 0) {
+  const current = oneday.date(date)
+  const weekday = current.day()
+  if (weekday === 0) {
     console.log('')
   }
   process.stdout.write(` ${String(date).padStart(2)}`)
