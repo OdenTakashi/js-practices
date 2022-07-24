b5694f4b39f5a5e3de253bb75da76e72d571b64a
OdenTakashi <odentakashi@icloud.com>
2022-07-24 12:07:40 +0900

    year,month再利用

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,11 +1,13 @@
 const dayjs = require('dayjs')
 const minimist = require('minimist')(process.argv.slice(2))
 
-const oneday = minimist.y ? dayjs(`${minimist.y}-${minimist.m}`) : dayjs()
+const year = minimist.y ? minimist.y : dayjs().year()
+const month = minimist.m ? minimist.m : dayjs().month() + 1
+const oneday = dayjs(`${year}-${month}`)
 const lastdate = oneday.endOf('month').date()
 const startweekday = oneday.day()
 
-console.log(`     ${oneday.month() + 1}月 ${oneday.year()}`)
+console.log(`     ${month}月 ${year}`)
 console.log(' 日 月 火 水 木 金 土')
 const startmargin = '   '.repeat(startweekday)
 process.stdout.write(`${startmargin}`)
@@ -18,3 +20,5 @@ for (let date = 1; date <= lastdate; date++) {
   }
   process.stdout.write(` ${String(date).padStart(2)}`)
 }
+
+console.log(dayjs().toDate().getTimezoneOffset())
