5a8f44d45c501388f6455a82e6defa106c1929ba
OdenTakashi <odentakashi@icloud.com>
2022-07-24 12:16:50 +0900

    指定した月を格納する変数名の変更

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -3,9 +3,9 @@ const minimist = require('minimist')(process.argv.slice(2))
 
 const year = minimist.y ? minimist.y : dayjs().year()
 const month = minimist.m ? minimist.m : dayjs().month() + 1
-const oneday = dayjs(`${year}-${month}`)
-const lastdate = oneday.endOf('month').date()
-const startweekday = oneday.day()
+const certainmonth = dayjs(`${year}-${month}`)
+const lastdate = certainmonth.endOf('month').date()
+const startweekday = certainmonth.day()
 
 console.log(`     ${month}月 ${year}`)
 console.log(' 日 月 火 水 木 金 土')
@@ -13,7 +13,7 @@ const startmargin = '   '.repeat(startweekday)
 process.stdout.write(`${startmargin}`)
 
 for (let date = 1; date <= lastdate; date++) {
-  const current = oneday.date(date)
+  const current = certainmonth.date(date)
   const weekday = current.day()
   if (weekday === 0) {
     console.log('')
