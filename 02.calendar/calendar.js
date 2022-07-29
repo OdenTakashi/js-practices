fe6e0505e3593feeb1847ffe9e67c28bee98c5ba
OdenTakashi <odentakashi@icloud.com>
2022-07-29 12:19:26 +0900

    変数名をスネークケースで記述

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -3,17 +3,17 @@ const minimist = require('minimist')(process.argv.slice(2))
 
 const year = minimist.y ? minimist.y : dayjs().year()
 const month = minimist.m ? minimist.m : dayjs().month() + 1
-const certainmonth = dayjs(`${year}-${month}`)
-const lastdate = certainmonth.endOf('month').date()
-const startweekday = certainmonth.day()
+const certain_month = dayjs(`${year}-${month}`)
+const last_date = certain_month.endOf('month').date()
+const start_weekday = certain_month.day()
 
 console.log(`     ${month}月 ${year}`)
 console.log(' 日 月 火 水 木 金 土')
-const startmargin = '   '.repeat(startweekday)
-process.stdout.write(`${startmargin}`)
+const start_margin = '   '.repeat(start_weekday)
+process.stdout.write(`${start_margin}`)
 
-for (let date = 1; date <= lastdate; date++) {
-  const current = certainmonth.date(date)
+for (let date = 1; date <= last_date; date++) {
+  const current = certain_month.date(date)
   const weekday = current.day()
   if (weekday === 0 && date !== 1) {
     console.log('')
