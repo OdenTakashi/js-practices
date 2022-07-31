45926fff9c0fa44d0cecef7c3b9c87c6ab07b8bb
OdenTakashi <odentakashi@icloud.com>
2022-07-31 13:55:58 +0900

    メモクラスを作成した

--- 04.class/class.js
+++ 04.class/class.js
@@ -4,65 +4,133 @@ const { exit } = require('process')
 const minimist = require('minimist')(process.argv.slice(2))
 const inputs = []
 
-// dオプション
-if (minimist.d) {
-  const files = fs.readdirSync('./memo_data', 'utf-8')
-  const base = files.map(file => {
-    return file.replace('.txt', '')
-  })
-  const question = {
-    type: 'select',
-    name: 'filename',
-    message: 'Choose a note you want to destroy:',
-    choices: base
-  }
-  prompt(question)
-    .then(answer => fs.unlinkSync(`./memo_data/${answer.filename}.txt`))
-}
+// // dオプション
+// if (minimist.d) {
+//   const files = fs.readdirSync('./memo_data', 'utf-8')
+//   const basename = files.map(file => {
+//     return file.replace('.txt', '')
+//   })
+//   const question = {
+//     type: 'select',
+//     name: 'filename',
+//     message: 'Choose a note you want to destroy:',
+//     choices: basename
+//   }
+//   prompt(question)
+//     .then(answer => fs.unlinkSync(`./memo_data/${answer.filename}.txt`))
+// }
 
-// rオプション
-if (minimist.r) {
-  const files = fs.readdirSync('./memo_data', 'utf-8')
-  const base = files.map(file => {
-    return file.replace('.txt', '')
-  })
-  const question = {
-    type: 'select',
-    name: 'filename',
-    message: 'Choose a note you want to see:',
-    choices: base
-  }
-  prompt(question)
-    .then(answer => console.log(fs.readFileSync(`./memo_data/${answer.filename}.txt`, 'utf-8')))
-}
+// // rオプション
+// if (minimist.r) {
+//   const files = fs.readdirSync('./memo_data', 'utf-8')
+//   const basename = files.map(file => {
+//     return file.replace('.txt', '')
+//   })
+//   const question = {
+//     type: 'select',
+//     name: 'filename',
+//     message: 'Choose a note you want to see:',
+//     choices: basename
+//   }
+//   prompt(question)
+//     .then(answer => console.log(fs.readFileSync(`./memo_data/${answer.filename}.txt`, 'utf-8')))
+// }
 
-// lオプション
-if (minimist.l) {
-  const filenames = fs.readdirSync('./memo_data', 'utf-8')
-  for (let number = 0; number < filenames.length; number++) {
-    console.log(filenames[number].replace('.txt', ''))
-  }
-  exit()
-}
+// // lオプション
+// if (minimist.l) {
+// const filenames = fs.readdirSync('./memo_data', 'utf-8')
+// for (let number = 0; number < filenames.length; number++) {
+//   console.log(filenames[number].replace('.txt', ''))
+// }
+// exit()
+// }
 
-// オプションなし
+// // オプションなし
 const reader = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout
 })
 
-if (!minimist.l && !minimist.r && !minimist.d) {
-  reader.on('line', function (line) {
-    inputs.push('\n' + line)
-  })
+// if (!minimist.l && !minimist.r && !minimist.d) {
+//   reader.on('line', function (line) {
+//     inputs.push('\n' + line)
+//   })
 
-  reader.question('メモを作成してね！ \n', (answer) => {
-    inputs.push(answer)
-  })
+//   reader.question('メモを作成してね！ \n', (answer) => {
+//     inputs.push(answer)
+//   })
 
-  reader.on('close', function () {
-    console.log('ファイルを保存しました。')
-    console.log(inputs.join(''))
-    fs.writeFileSync(`./memo_data/${inputs[0]}.txt`, inputs.join(''))
-  })
+//   reader.on('close', function () {
+//     console.log('ファイルを保存しました。')
+//     console.log(inputs.join(''))
+//     fs.writeFileSync(`./memo_data/${inputs[0]}.txt`, inputs.join(''))
+//   })
+// }
+
+class Memo {
+  create () {
+    reader.on('line', function (line) {
+      inputs.push('\n' + line)
+    })
+
+    reader.question('メモを作成してね！ \n', (answer) => {
+      inputs.push(answer)
+    })
+
+    reader.on('close', function () {
+      console.log('ファイルを保存しました。')
+      console.log(inputs.join(''))
+      fs.writeFileSync(`./memo_data/${inputs[0]}.txt`, inputs.join(''))
+    })
+  }
+
+  delete () {
+    const files = fs.readdirSync('./memo_data', 'utf-8')
+    const basename = files.map(file => {
+      return file.replace('.txt', '')
+    })
+    const question = {
+      type: 'select',
+      name: 'filename',
+      message: 'Choose a note you want to destroy:',
+      choices: basename
+    }
+    prompt(question)
+      .then(answer => fs.unlinkSync(`./memo_data/${answer.filename}.txt`))
+  }
+
+  list () {
+    const filenames = fs.readdirSync('./memo_data', 'utf-8')
+    for (let number = 0; number < filenames.length; number++) {
+      console.log(filenames[number].replace('.txt', ''))
+    }
+    exit()
+  }
+
+  show () {
+    const files = fs.readdirSync('./memo_data', 'utf-8')
+    const basename = files.map(file => {
+      return file.replace('.txt', '')
+    })
+    const question = {
+      type: 'select',
+      name: 'filename',
+      message: 'Choose a note you want to see:',
+      choices: basename
+    }
+    prompt(question)
+      .then(answer => console.log(fs.readFileSync(`./memo_data/${answer.filename}.txt`, 'utf-8')))
+  }
+}
+
+const memo = new Memo()
+
+if (minimist.d) {
+  memo.delete()
+} else if (minimist.l) {
+  memo.list()
+} else if (minimist.r) {
+  memo.show()
+} else {
+  memo.create()
 }
